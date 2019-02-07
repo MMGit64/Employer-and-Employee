ea316a60ff6f68af0cc97a487b3a1cf7b9a52765
Mustafa Mossa <44642964+MMGit64@users.noreply.github.com>
2019-02-07 15:02:28 +0000

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -4,12 +4,16 @@ public class Employee {
 	private String name; 
 	private int id = 0;
 	private int age; 
+	private String gender;
+	private String ethBackground;
 	
-	public Employee(String emp_name, int emp_age, int emp_id){
+	public Employee(String emp_name, int emp_age, int emp_id, String emp_gender, String emp_eth_Background){
 		
 		name = emp_name; 
 		age = emp_age;
 		id = emp_id;
+		gender = emp_gender;
+		ethBackground = emp_eth_Background
 	}
 	
 	public int getAge(){
@@ -25,12 +29,17 @@ public class Employee {
 		return name;
 	}
 	
-	//public void setFirstName(String fn){
-		//name = fn;
-	//}
+	public String getGender(){
+		return gender;
+	}	
+	
+	public String getEthBackground(){
+		return ethBackground;
+	}
+
 	
 	public String toString(){
-		return String.format("(%s, %d, %d)", name, age, id);
+		return String.format("(%s, %d, %d)", name, age, id, gender, ethBackground);
 	}
 }	
 	
